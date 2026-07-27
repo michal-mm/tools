e4f60113945d3273c240d633b5411b8faa89c49d
michal-mm <michal.miotk@gmail.com>
2026-07-27 20:03:28 +0200

    remove unused imports

--- src/main/java/michalmm/jtools/RenameFile.java
+++ src/main/java/michalmm/jtools/RenameFile.java
@@ -2,7 +2,6 @@ package michalmm.jtools;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.stream.Stream;
